aaa2e5065fa1da91789ef86eba89734355c56364
NehaDhanwani <dhanwanineha@gmail.com>
2014-02-24 12:25:16 -0800

    Added info option on start and finish screens

--- FlashCardsTemplate/source/FlashCardsTemplateApp/src/com/buildmlearn/flashcard/ScoreActivity.java
+++ FlashCardsTemplate/source/FlashCardsTemplateApp/src/com/buildmlearn/flashcard/ScoreActivity.java
@@ -1,13 +1,18 @@
 package com.buildmlearn.flashcard;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 
 public class ScoreActivity extends SherlockActivity {
 	GlobalData gd;
@@ -42,6 +47,49 @@ public class ScoreActivity extends SherlockActivity {
 				// android.os.Process.killProcess(android.os.Process.myPid());
 			}
 		});
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		getSupportMenuInflater().inflate(R.menu.main, menu);
+
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.action_info) {
+
+			AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+					ScoreActivity.this);
+
+			// set title
+			alertDialogBuilder.setTitle("About Us");
+
+			// set dialog message
+			alertDialogBuilder
+					.setMessage(getString(R.string.about_us))
+					.setCancelable(false)
+					.setPositiveButton("Ok",
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,
+										int id) {
+									dialog.cancel();
+									dialog.dismiss();
+								}
+							});
+
+			// create alert dialog
+			AlertDialog alertDialog = alertDialogBuilder.create();
+			// show it
+			alertDialog.show();
+			TextView msg = (TextView) alertDialog
+					.findViewById(android.R.id.message);
+			Linkify.addLinks(msg, Linkify.WEB_URLS);
+
+			return super.onOptionsItemSelected(item);
+		}
+		return true;
 	}
 
 }
