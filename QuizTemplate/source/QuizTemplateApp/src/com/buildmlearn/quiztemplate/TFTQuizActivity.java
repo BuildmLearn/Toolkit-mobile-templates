456b1d1dc7b5f703dc1deb1d1e60e827b442568c
NehaDhanwani <dhanwanineha@gmail.com>
2014-02-24 12:21:25 -0800

    Added info option on start and finish screens

--- QuizTemplate/source/QuizTemplateApp/src/com/buildmlearn/quiztemplate/TFTQuizActivity.java
+++ QuizTemplate/source/QuizTemplateApp/src/com/buildmlearn/quiztemplate/TFTQuizActivity.java
@@ -4,14 +4,14 @@ All rights reserved.
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
 
-* Redistributions of source code must retain the above copyright notice, this
+ * Redistributions of source code must retain the above copyright notice, this
   list of conditions and the following disclaimer.
 
-* Redistributions in binary form must reproduce the above copyright notice,
+ * Redistributions in binary form must reproduce the above copyright notice,
   this list of conditions and the following disclaimer in the documentation
   and/or other materials provided with the distribution.
 
-* Neither the name of the BuildmLearn nor the names of its
+ * Neither the name of the BuildmLearn nor the names of its
   contributors may be used to endorse or promote products derived from
   this software without specific prior written permission.
 
@@ -26,32 +26,37 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
-
 package com.buildmlearn.quiztemplate;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 
 public class TFTQuizActivity extends SherlockActivity {
 	private GlobalData gd;
-	
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//getWindow().requestFeature(Window.FEATURE_ACTION_BAR); // Add this line
+		// getWindow().requestFeature(Window.FEATURE_ACTION_BAR); // Add this
+		// line
 
 		setContentView(R.layout.start_view);
 
 		gd = GlobalData.getInstance();
 		reInitialize();
-		
+
 		gd.ReadContent(TFTQuizActivity.this);
 
 		TextView quizAuthor = (TextView) findViewById(R.id.tv_author);
@@ -65,7 +70,7 @@ public class TFTQuizActivity extends SherlockActivity {
 
 			@Override
 			public void onClick(View arg0) {
-					Intent myIntent = new Intent(arg0.getContext(),
+				Intent myIntent = new Intent(arg0.getContext(),
 						QuestionActivity.class);
 				startActivity(myIntent);
 				finish();
@@ -74,11 +79,55 @@ public class TFTQuizActivity extends SherlockActivity {
 
 	}
 
-	private void reInitialize()
-	{
-		gd.total=0;
-		gd.correct=0;
-		gd.wrong=0;
+	private void reInitialize() {
+		gd.total = 0;
+		gd.correct = 0;
+		gd.wrong = 0;
 		gd.iQuizList.clear();
 	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		getSupportMenuInflater().inflate(R.menu.main, menu);
+
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.action_info) {
+
+			AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+					TFTQuizActivity.this);
+
+			// set title
+			alertDialogBuilder.setTitle("About Us");
+
+			// set dialog message
+			alertDialogBuilder
+					.setMessage(getString(R.string.about_us))
+					.setCancelable(false)
+					.setPositiveButton("Ok",
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,
+										int id) {
+									dialog.cancel();
+									dialog.dismiss();
+								}
+							});
+
+			// create alert dialog
+			AlertDialog alertDialog = alertDialogBuilder.create();
+			// show it
+			alertDialog.show();
+			TextView msg = (TextView) alertDialog
+					.findViewById(android.R.id.message);
+			Linkify.addLinks(msg, Linkify.WEB_URLS);
+
+			return super.onOptionsItemSelected(item);
+		}
+		return true;
+	}
+
 }
