faeac9b6137a0dc69b3d4aceed80fac7d1c0c921
Anupam Das <anupam.das.bwn@gmail.com>
2016-05-12 00:13:50 +0530

    Added Main Activity

---
+++ VideoCollectionTemplate/source/VideoCollectionTemplateApp/app/src/main/java/org/buildmlearn/videocollection/activities/MainActivity.java
@@ -0,0 +1,18 @@
+package org.buildmlearn.videocollection.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.buildmlearn.videocollection.R;
+
+/**
+ * Created by Anupam (opticod) on 11/5/16.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
