68fcc093d36dffbedee30783e09754b15583c77a
Anupam Das <anupam.das.bwn@gmail.com>
2016-05-14 01:48:55 +0530

    Enable SplashActivity to get real data from XML

--- VideoCollectionTemplate/source/VideoCollectionTemplateApp/app/src/main/java/org/buildmlearn/videocollection/activities/SplashActivity.java
+++ VideoCollectionTemplate/source/VideoCollectionTemplateApp/app/src/main/java/org/buildmlearn/videocollection/activities/SplashActivity.java
@@ -4,8 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+import org.buildmlearn.videocollection.Constants;
 import org.buildmlearn.videocollection.R;
+import org.buildmlearn.videocollection.data.DataUtils;
 
 /**
  * Created by Anupam (opticod) on 11/5/16.
@@ -17,10 +20,19 @@ public class SplashActivity extends Activity {
         super.onCreate(savedInstanceState);
         final Activity mActivity = this;
         setContentView(R.layout.activity_splash);
+
+        final String result[] = DataUtils.read_Title_Author(this, Constants.XMLFileName);
+        TextView title = (TextView) findViewById(R.id.title);
+        TextView author_name = (TextView) findViewById(R.id.author_name);
+
+        title.setText(result[0]);
+        author_name.setText(result[1]);
+
         findViewById(R.id.enter).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(mActivity, MainActivity.class);
+                Intent intent = new Intent(mActivity, MainActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, result[0]);
                 startActivity(intent);
                 finish();
             }
