789f76b35b67eb8ad074e77b958c09abe7fbb7cc
Anupam Das <anupam.das.bwn@gmail.com>
2016-05-14 03:26:44 +0530

    Added DetailActivity to show details of any video

---
+++ VideoCollectionTemplate/source/VideoCollectionTemplateApp/app/src/main/java/org/buildmlearn/videocollection/activities/DetailActivity.java
@@ -0,0 +1,50 @@
+package org.buildmlearn.videocollection.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.buildmlearn.videocollection.R;
+import org.buildmlearn.videocollection.fragment.DetailActivityFragment;
+
+/**
+ * Created by Anupam (opticod) on 11/5/16.
+ */
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        Bundle extras = getIntent().getExtras();
+        String videoId = null;
+        if (extras != null) {
+            videoId = extras.getString(Intent.EXTRA_TEXT);
+        }
+        if (savedInstanceState == null) {
+            Bundle arguments = new Bundle();
+            arguments.putString(Intent.EXTRA_TEXT, videoId);
+
+            DetailActivityFragment fragment = new DetailActivityFragment();
+            fragment.setArguments(arguments);
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.video_detail_container, fragment)
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
