7c475ea6f1d1263777c91e464a917da8552ba487
Anupam Das <anupam.das.bwn@gmail.com>
2016-06-01 00:00:23 +0530

    Added SplashActivity.java

---
+++ ComprehensionTemplate/source/Comprehension/app/src/main/java/org/buildmlearn/comprehension/activities/SplashActivity.java
@@ -0,0 +1,54 @@
+package org.buildmlearn.comprehension.activities;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.buildmlearn.comprehension.Constants;
+import org.buildmlearn.comprehension.R;
+import org.buildmlearn.comprehension.data.DataUtils;
+import org.buildmlearn.comprehension.data.FetchXMLTask;
+
+/**
+ * Created by Anupam (opticod) on 31/5/16.
+ */
+public class SplashActivity extends Activity {
+
+    private SharedPreferences prefs = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final Activity mActivity = this;
+        setContentView(R.layout.activity_splash);
+
+        final String result[] = DataUtils.read_Title_Author(this);
+        TextView title = (TextView) findViewById(R.id.title);
+        TextView author_name = (TextView) findViewById(R.id.author_name);
+
+        title.setText(result[0]);
+        author_name.setText(result[1]);
+
+        findViewById(R.id.enter).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        prefs = getSharedPreferences(Constants.firstrun, MODE_PRIVATE);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (prefs.getBoolean(Constants.firstrun, true)) {
+            FetchXMLTask xmlTask = new FetchXMLTask(this);
+            xmlTask.execute(Constants.XMLFileName);
+            prefs.edit().putBoolean(Constants.firstrun, false).apply();
+        }
+    }
+}
