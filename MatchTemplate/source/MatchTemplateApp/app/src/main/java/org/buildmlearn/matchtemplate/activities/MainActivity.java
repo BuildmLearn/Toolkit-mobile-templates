2da03c2148a9a82912f62a12d4901e7f2058d074
Anupam Das <anupam.das.bwn@gmail.com>
2016-07-26 19:32:10 +0530

    Added menu to show app info

--- MatchTemplate/source/MatchTemplateApp/app/src/main/java/org/buildmlearn/matchtemplate/activities/MainActivity.java
+++ MatchTemplate/source/MatchTemplateApp/app/src/main/java/org/buildmlearn/matchtemplate/activities/MainActivity.java
@@ -1,7 +1,12 @@
 package org.buildmlearn.matchtemplate.activities;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.LinkMovementMethod;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import org.buildmlearn.matchtemplate.R;
 
@@ -17,4 +22,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+
+            case R.id.action_about:
+                AlertDialog.Builder builder =
+                        new AlertDialog.Builder(this);
+                builder.setTitle(String.format("%1$s", getString(R.string.about_us)));
+                builder.setMessage(getResources().getText(R.string.about_text));
+                builder.setPositiveButton(getString(R.string.ok), null);
+                AlertDialog welcomeAlert = builder.create();
+                welcomeAlert.show();
+                ((TextView) welcomeAlert.findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance());
+
+                break;
+            default: //do nothing
+                break;
+        }
+        return (super.onOptionsItemSelected(menuItem));
+    }
 }
