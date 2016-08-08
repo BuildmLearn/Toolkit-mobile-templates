328155f1a6f4782b5948083be615c1699e014ebc
Anupam Das <anupam.das.bwn@gmail.com>
2016-08-08 10:50:55 +0530

    Added data files

---
+++ FlashCardsTemplate/source/FlashCardsTemplateAppV3/app/src/main/java/org/buildmlearn/learnwithflashcards/data/DataUtils.java
@@ -0,0 +1,43 @@
+package org.buildmlearn.learnwithflashcards.data;
+
+import android.content.Context;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+/**
+ * Created by Anupam (opticod) on 7/7/16.
+ */
+public class DataUtils {
+
+    public static String[] readTitleAuthor(Context myContext) {
+        String result[] = new String[2];
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+        dbf.setValidating(false);
+
+        DocumentBuilder db;
+        Document doc;
+        try {
+            db = dbf.newDocumentBuilder();
+            doc = db.parse(myContext.getAssets().open(org.buildmlearn.learnwithflashcards.Constants.XMLFileName));
+            doc.normalize();
+
+            result[0] = doc.getElementsByTagName("title").item(0).getChildNodes()
+                    .item(0).getNodeValue();
+
+            result[1] = doc.getElementsByTagName("name").item(0).getChildNodes()
+                    .item(0).getNodeValue();
+
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
