6c5c1504e9c37d1173cf40ac84de852b599441b2
Anupam Das <anupam.das.bwn@gmail.com>
2016-08-08 10:51:18 +0530

    Added constants + animations + widgets

---
+++ FlashCardsTemplate/source/FlashCardsTemplateAppV3/app/src/main/java/org/buildmlearn/learnwithflashcards/Constants.java
@@ -0,0 +1,15 @@
+package org.buildmlearn.learnwithflashcards;
+
+/**
+ * Created by Anupam (opticod) on 7/7/16.
+ */
+public class Constants {
+    public static final String XMLFileName = "flash_content.xml";
+    public static final String firstrun = "firstRun";
+
+    public static final int COL_QUESTION = 1;
+    public static final int COL_ANSWER = 2;
+    public static final int COL_HINT = 3;
+    public static final int COL_BASE64 = 4;
+
+}
