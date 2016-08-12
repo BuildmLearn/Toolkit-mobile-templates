1cc3ee069da1c4f35e6661b5e69781718faa0ea1
Anupam Das <anupam.das.bwn@gmail.com>
2016-08-12 23:39:40 +0530

    Added models + constants

---
+++ QuizTemplate/source/QuizTemplateAppV3/app/src/main/java/org/buildmlearn/quiztime/Constants.java
@@ -0,0 +1,19 @@
+package org.buildmlearn.quiztime;
+
+/**
+ * Created by Anupam (opticod) on 11/8/16.
+ */
+public class Constants {
+    public static final String XMLFileName = "quiz_content.xml";
+    public static final String firstrun = "firstRun";
+
+    public static final int COL_QUESTION = 1;
+    public static final int COL_OPTION_1 = 2;
+    public static final int COL_OPTION_2 = 3;
+    public static final int COL_OPTION_3 = 4;
+    public static final int COL_OPTION_4 = 5;
+    public static final int COL_CORRECT_ANSWER = 6;
+    public static final int COL_ANSWERED = 7;
+    public static final int COL_ATTEMPTED = 8;
+
+}
